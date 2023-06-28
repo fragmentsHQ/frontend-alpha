322298dd98a27faa8377928a674116e1e2bbe149
Ketan <krotusppp@gmail.com>
2023-06-28 23:45:38 +0530

    fix: remove tests + update name

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,15 +5,6 @@ export default function Header() {
   return (
     <header className='z-50'>
       <div className=' flex h-[15vh] items-center justify-end'>
-        {/* <UnstyledLink href='/' className='font-bold hover:text-gray-600'>
-          <div className='relative h-[38px] w-[30px] cursor-pointer md:h-[48px] md:w-[40px]'>
-            <Image
-              src={require('../../../public/images/valent-white.png')}
-              alt='valent'
-              fill
-            />
-          </div>
-        </UnstyledLink> */}
         <ConnectButton />
       </div>
     </header>
