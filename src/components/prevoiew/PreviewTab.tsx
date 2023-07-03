df48c29dbff3a296d2332c22767973b35f47d127
Ketan kudikyal <66976099+KetanKudikyal@users.noreply.github.com>
2023-07-03 14:28:19 +0530

    fix: disable forward paying gas

--- src/components/prevoiew/PreviewTab.tsx
+++ src/components/prevoiew/PreviewTab.tsx
@@ -2,11 +2,13 @@ const options = [
   {
     id: 1,
     name: 'Forward paying gas ',
+    isDisabled: true,
     value: 'forwardpaying',
   },
   {
     id: 2,
     name: 'Pay from gas account ',
+    isDisabled: false,
     value: 'gassaccount',
   },
 ];
@@ -22,17 +24,21 @@ const PreviewTabMenu: React.FC = () => {
   return (
     <div className='flex space-x-3 rounded-[16px] bg-[#373A40] p-2'>
       {options.map((option) => (
-        <a
+        <button
           key={option.value}
           className={`flex w-full flex-1  cursor-pointer items-center justify-center rounded-[12px] py-2  text-[16px] transition-all  duration-200 focus:outline-none ${
             paymentMethod === option.value
               ? 'bg-[#0047CE] font-medium text-white'
               : 'bg-[#262229]  text-white'
+          } ${
+            option.isDisabled &&
+            'disabled:cursor-not-allowed disabled:bg-[#262229] disabled:bg-opacity-50 disabled:text-opacity-20'
           }`}
           onClick={() => setPaymentMethod(option.value)}
+          disabled={option.isDisabled}
         >
           {option.name}
-        </a>
+        </button>
       ))}
     </div>
   );
